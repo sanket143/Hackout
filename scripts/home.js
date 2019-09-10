d1369631185a84b438b54ec4885c251fdf23f938
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2019-09-11 01:29:58 +0530

    Initial Commit

---
+++ scripts/home.js
@@ -0,0 +1,9 @@
+$(window).on('load',
+    function(){
+        setTimeout(function(){
+            $(".loader").fadeOut();
+            $(".aHackout").addClass("a_Hackout");
+            $(".a2019").addClass("a_2019");
+    }, 1000)
+    }
+)
